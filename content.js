618353284dbd0d64b5c108230d505ec8ca679c98
fedeglggg <niemil9326@gmail.com>
2022-01-09 06:04:37 -0300

    folder structure and preparing 2nd modal

--- content.js
+++ content.js
@@ -47,11 +47,9 @@ function add_modal() {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-
         <h2 class="modal-title" id="exampleModalLabel">Enviar lead de whatsapp a tecnom</h2>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-    
       <div class="modal-body">
         <form id="modal_form" action="/">
           <div class="mb-3">
@@ -66,15 +64,13 @@ function add_modal() {
           </div>  
         </form>
       </div>
-      
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
         <button type="button" class="btn btn-primary">Enviar</button>
       </div>
-      
     </div>
   </div>
-</div>
+  </div>
   `;
   
   let div = document.createElement("div");
